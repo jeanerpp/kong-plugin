ad048309bcf92642a7bba32d846f321a9cdb5e22
xujian <jian.sh.xu@outlook.com>
2026-03-30 06:08:21 +0000

    test cache be invalided if plugin configure changed

--- spec/myplugin/10-integration_spec.lua
+++ spec/myplugin/10-integration_spec.lua
@@ -1,4 +1,5 @@
 local helpers = require "spec.helpers"
+local cjson = require "cjson"
 
 
 local PLUGIN_NAME = "myplugin"
@@ -11,6 +12,7 @@ local AUTH_SERVER = "http://" .. PONGO_NETWORK .. "-nginx-remote-ok." .. PONGO_N
 for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
   describe(PLUGIN_NAME .. ": (access) [#" .. strategy .. "]", function()
     local client
+    local plugin_id  -- Save plugin ID for Admin API updates
 
     lazy_setup(function()
 
@@ -19,10 +21,10 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
       -- Inject a test route. No need to create a service, there is a default
       -- service which will echo the request.
       local route1 = bp.routes:insert({
-        hosts = { "test1.com", "test2.com" },
+        hosts = { "test1.com", "test2.com", "test3.com" },
       })
       -- add the plugin to test to the route we created
-      bp.plugins:insert {
+      local plugin = bp.plugins:insert {
         name = PLUGIN_NAME,
         route = { id = route1.id },
         config = {
@@ -32,6 +34,7 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
           ttl = 10,
         },
       }
+      plugin_id = plugin.id
 
       -- start kong
       assert(helpers.start_kong({
@@ -102,6 +105,74 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
 
       end)
     end)
+
+
+    -- Cache invalidation via Admin API only works with a database
+    if strategy ~= "off" then
+      describe("cache invalidation", function()
+        it("invalidates cache when plugin config changes", function()
+          -- First request: cache miss with original config
+          local r1 = client:get("/request", {
+            headers = { host = "test3.com" }
+          })
+          assert.response(r1).has.status(200)
+          assert.response(r1).has.no.header("X-Cache-Status")
+
+          -- Second request: cache hit with same config
+          local r2 = client:get("/request", {
+            headers = { host = "test3.com" }
+          })
+          assert.response(r2).has.status(200)
+          local cache_status2 = assert.response(r2).has.header("X-Cache-Status")
+          assert.equal("HIT", cache_status2)
+
+          -- Update plugin configuration via Admin API
+          -- This triggers plugin:configure which increments cache_version
+          local admin = helpers.admin_client()
+          local res = admin:patch("/plugins/" .. plugin_id, {
+            headers = { ["Content-Type"] = "application/json" },
+            body = cjson.encode({
+              config = {
+                request_header_name = "X-My-Header",
+                remote_auth_server = AUTH_SERVER,
+                auth_header_name = "Authorization",
+                ttl = 10,
+              }
+            })
+          })
+          assert.res_status(200, res)
+          admin:close()
+
+          -- Wait for Kong to reload the updated config
+          helpers.wait_for_all_config_update()
+
+          -- Third request: should be cache miss due to config change
+          local client3 = helpers.proxy_client()
+          local r3 = client3:get("/request", {
+            headers = {
+              host = "test3.com",
+              ["X-My-Header"] = "test-value"
+            }
+          })
+          assert.response(r3).has.status(200)
+          assert.response(r3).has.no.header("X-Cache-Status")
+          client3:close()
+
+          -- Fourth request: cache hit with new config
+          local client4 = helpers.proxy_client()
+          local r4 = client4:get("/request", {
+            headers = {
+              host = "test3.com",
+              ["X-My-Header"] = "test-value"
+            }
+          })
+          assert.response(r4).has.status(200)
+          local cache_status4 = assert.response(r4).has.header("X-Cache-Status")
+          assert.equal("HIT", cache_status4)
+          client4:close()
+        end)
+      end)
+    end -- strategy ~= "off"
 
   end)
 
